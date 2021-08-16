adec1296a6ac8e23d7e8f6c61326c577419dcd94
noobstonks <androfarlinno@gmail.com>
2021-08-16 15:08:51 +0700

    buat form untuk registrasi with view engine

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+const users =[]
 
+//nge-parse form dari halaman sebelumnya
+app.use (express.urlencoded({extended: false}))
 
 app.set('view engine', 'ejs');
-// app.set("view engine", "ejs");
 
-app.get ('/', (req, res)=>{
-    res.render('index')
+//menampilkan jumlah user
+app.get('/', (req,res) =>{
+    res.send(`jumlah usernya:  ${users.length}`)
 })
 
-  app.listen(8000, () =>{
-      console.log("server nyala")
-  })
+//register page untuk menampilkan form register
+app.get ('/register', (req,res)=>{
+    res.render('register')
+})
 
-app.get('/greet', (req,res)=>{
-    const name = req.query.name || 'void'
-    res.render('greet', {
-        name
-    })
+// post / register untuk melakukan registrasi user
+app.post('/register', (req,res)=>{
+    const {email, password} = req.body
+    users.push({email,password})
+    res.redirect('/')
+})
+
+//test view engine sebelumnya
+// app.get ('/', (req, res)=>{
+//     res.render('index')
+// })
+
+
+
+ app.get('/greet', (req,res)=>{
+     const name = req.query.name || 'void'
+     res.render('greet', {
+         name
+     })
+ })
+
+
+app.listen(8000, () =>{
+    console.log("server nyala")
 })
 
 
